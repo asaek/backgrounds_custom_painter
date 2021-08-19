0cb84f1ef9a8b645fac0df9c5675c1a89b1cd6af
Asaek <Asaek60@gmail.com>
2021-08-19 07:16:14 -0500

    Finalizacion del ejercicio

--- lib/screens/slideshow2_screen.dart
+++ lib/screens/slideshow2_screen.dart
@@ -6,21 +6,35 @@ class SlideShowScreen2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SlideShowList(
-        dotActivo: 20,
-        dotDesactivado: 10,
-        // dotsArriba: true,
-        // colorPrimario: Colors.red,
-        // colorSecundario: Colors.purpleAccent,
-        // Con esto puedes enviar lo que quieras mientras sea un Widget
-        slidesList: [
-          SvgPicture.asset('assets/slice1.svg'),
-          SvgPicture.asset('assets/slice2.svg'),
-          SvgPicture.asset('assets/slice3.svg'),
-          SvgPicture.asset('assets/slice4.svg'),
-          SvgPicture.asset('assets/slice1.svg'),
-        ],
-      ),
+      body: Column(children: [
+        Expanded(child: MiSlideshow()),
+        Expanded(child: MiSlideshow()),
+      ]),
+    );
+  }
+}
+
+class MiSlideshow extends StatelessWidget {
+  const MiSlideshow({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideShowList(
+      dotActivo: 20,
+      dotDesactivado: 10,
+      // dotsArriba: true,
+      // colorPrimario: Colors.red,
+      // colorSecundario: Colors.purpleAccent,
+      // Con esto puedes enviar lo que quieras mientras sea un Widget
+      slidesList: [
+        SvgPicture.asset('assets/slice1.svg'),
+        SvgPicture.asset('assets/slice2.svg'),
+        SvgPicture.asset('assets/slice3.svg'),
+        SvgPicture.asset('assets/slice4.svg'),
+        SvgPicture.asset('assets/slice1.svg'),
+      ],
     );
   }
 }
